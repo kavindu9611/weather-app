7c4b995c30ccf9676c15cf813e80db528cb6155c
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2025-11-16 03:54:39 +0530

    feat:Add '.env' file to the backend and import 'PORT' number

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express();
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT,()=>{
   console.log(`Backend running at http://localhost:${PORT}`)
