8b2f704c546ce256914bd11a4e13071e1030743b
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2025-11-17 06:21:33 +0530

    fix

--- backend/src/services/weatherService.ts
+++ backend/src/services/weatherService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getCache, setCache } from "../cache";
 
-const API_KEY = process.env.OPENWEATHER_API_KEY;
+const API_KEY = process.env.OPENWEATHER_API_KEY || "38362449fce13e8bcd4a6bc7e4f8596e";
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 export const fetchWeatherById = async (cityId: string): Promise<any> => {
